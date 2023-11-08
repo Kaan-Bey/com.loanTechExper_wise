ab439e0940a2911b0ca17474d63305e1b988d94d
Ozge Bayraktar <byrktr.oske@gmail.com>
2023-11-08 10:16:39 +0300

    Query olusturuldu

--- src/test/java/utilities/Manage.java
+++ src/test/java/utilities/Manage.java
@@ -1,4 +1,11 @@
 package utilities;
 
 public class Manage {
+
+
+    public String getQuery18() {
+        return Query18;
+    }
+
+    private  String Query18="UPDATE users set mobile =? where username LIKE '%e_';";
 }
