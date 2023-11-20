8f96d68c4049bcc8af6756e2fa1606cb09cfb0e8
Recep ALICI <alicirecepp@gmail.com>
2023-11-20 22:51:17 +0100

    recep/20/11/2023/V1

--- src/test/java/runners/DB_Runner.java
+++ src/test/java/runners/DB_Runner.java
@@ -14,7 +14,7 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/DB",
         glue = {"stepdefinitions"},
-        tags = "@DB18",
+        tags = "@F1",
         dryRun =false
 )
 public class DB_Runner {
